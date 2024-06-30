307c6e604cdfae40ee8d6e46589a01bd24518bcc
LeroyM1 <jolee2@gmail.com>
2024-06-30 12:21:23 +0300

    Added Lombok

--- src/main/java/com/example/Timesheet/model/Project.java
+++ src/main/java/com/example/Timesheet/model/Project.java
@@ -1,10 +1,13 @@
 package com.example.Timesheet.model;
 
 import jakarta.persistence.*;
+import lombok.Data;
+import org.apache.logging.log4j.util.StringMap;
 
 import java.util.Set;
 
 @Entity
+@Data
 public class Project {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -19,23 +22,4 @@ public class Project {
         return id;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Set<Timesheet> getTimesheets() {
-        return timesheets;
-    }
-
-    public void setTimesheets(Set<Timesheet> timesheets) {
-        this.timesheets = timesheets;
-    }
 }
--- src/main/java/com/example/Timesheet/model/Timesheet.java
+++ src/main/java/com/example/Timesheet/model/Timesheet.java
@@ -1,8 +1,11 @@
 package com.example.Timesheet.model;
 import jakarta.persistence.*;
+import lombok.Data;
+
 import java.time.LocalDate;
 
 @Entity
+@Data
 public class Timesheet {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -20,51 +23,4 @@ public class Timesheet {
     @JoinColumn(name = "project_id")
     private Project project;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public LocalDate getDate() {
-        return date;
-    }
-
-    public void setDate(LocalDate date) {
-        this.date = date;
-    }
-
-    public int getHours() {
-        return hours;
-    }
-
-    public void setHours(int hours) {
-        this.hours = hours;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
-
-    public Project getProject() {
-        return project;
-    }
-
-    public void setProject(Project project) {
-        this.project = project;
-    }
 }
