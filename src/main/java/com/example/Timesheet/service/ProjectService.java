3117bbb74a5929191d3624be4e78aac4c7ad1b7f
LeroyM1 <jolee2@gmail.com>
2024-07-12 17:06:05 +0300

    Updated ProjectService

--- src/main/java/com/example/Timesheet/service/ProjectService.java
+++ src/main/java/com/example/Timesheet/service/ProjectService.java
@@ -2,15 +2,17 @@ package com.example.Timesheet.service;
 
 import com.example.Timesheet.entity.Project;
 import com.example.Timesheet.repository.ProjectRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class ProjectService {
-    @Autowired
-    private ProjectRepository projectRepository;
+
+    private final ProjectRepository projectRepository;
 
     public Project createProject(Project project) {
         return projectRepository.save(project);
