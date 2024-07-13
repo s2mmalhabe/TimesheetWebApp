4f0a33eed6b93aa798ebef2ad145d19f26846ab5
LeroyM1 <jolee2@gmail.com>
2024-07-13 10:55:27 +0300

    Updated Component and app.routes.ts

--- frontend/angular-app/src/app/app.routes.ts
+++ frontend/angular-app/src/app/app.routes.ts
@@ -1,3 +1,5 @@
 import { Routes } from '@angular/router';
+import {HomeComponent} from "./home";
 
-export const routes: Routes = [];
+export const routes: Routes = [{path: '', component: HomeComponent}];
+
