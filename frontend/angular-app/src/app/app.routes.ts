cf77cda92be13897cba1fe71fb64f1e054a16386
LeroyM1 <jolee2@gmail.com>
2024-07-13 11:49:32 +0300

    Updated app.routes.ts

--- frontend/angular-app/src/app/app.routes.ts
+++ frontend/angular-app/src/app/app.routes.ts
@@ -1,6 +1,16 @@
 import { Routes } from '@angular/router';
 import {HomeComponent} from "./home";
+import {AboutUsComponent} from "./aboutUs";
+import {ContactComponent} from "./contact";
+import {GalleryComponent} from "./gallery";
+import {LoginComponent} from "./login";
+import {PreviousProjectsComponent} from "./previousProjects";
 
 export const routes: Routes = [
-  {path: '', component: HomeComponent}
+  {path: '', component: HomeComponent},
+  {path: 'about-us', component: AboutUsComponent},
+  {path: 'contact', component: ContactComponent},
+  {path: 'gallery', component: GalleryComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'previousProjects', component: PreviousProjectsComponent},
 ];
