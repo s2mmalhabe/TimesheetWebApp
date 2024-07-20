4b07e7a7e14f78362fc6d106eae8dcad7ece7868
LeroyM1 <jolee2@gmail.com>
2024-07-21 00:57:22 +0300

    login.component.ts cleanup

--- frontend/angular-app/src/app/component/login/login.component.ts
+++ frontend/angular-app/src/app/component/login/login.component.ts
@@ -1,18 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { UserService } from '../../service/user.service';
-// import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Credentials } from '../../model/credentials';
-import {HttpClient} from "@angular/common/http";
 import {Router} from "@angular/router";
 
 @Component({
   selector: 'login',
   templateUrl: 'login.component.html' })
-export class LoginComponent implements OnInit {
-  // loginForm: FormGroup = new FormGroup({
-  //   email: new FormControl(),
-  //   password: new FormControl()
-  // });
+export class LoginComponent {
   credentials: Credentials = new class implements Credentials {
     email: string = "";
     password: string = "";
@@ -21,43 +15,15 @@ export class LoginComponent implements OnInit {
   constructor(
     private userService: UserService,
     private router: Router
-
-    // private formBuilder: FormBuilder
   ) {}
 
-  ngOnInit() {
-    // console.log('init')
-    // this.reactiveForm();
-  }
-
-  reactiveForm() {
-    // this.loginForm = this.formBuilder.group({
-    //   email: ['', [
-    //     Validators.required,
-    //     Validators.email
-    //   ]],
-    //   password: ['', [
-    //     Validators.required
-    //   ]]
-    // });
-    // console.log('reactiveForm')
-  }
-
-  errorHandling(control: string, error: string) {
-    // return this.loginForm.controls[control].hasError(error);
-  }
-
   login(): void {
-    // console.log(this.loginForm.valid)
-    console.log('login')
-    // this.credentials.email = this.loginForm.value.email.trim();
-    // this.credentials.password = this.loginForm.value.password.trim();
     this.userService.login(this.credentials)
       .subscribe(response => {
         console.log('success: '+response.success);
         console.log('message: '+response.message);
         if (response.success)
           this.router.navigate(['timesheet']);
-      })
+      });
   }
 }
